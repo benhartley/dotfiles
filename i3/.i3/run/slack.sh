6c57b5afc6a493c2f7c7e001ea37040feea5b7c8
Benedict Hartley <spam@chaffin.ch>
2019-09-18 15:33:05 +0100

    i3 slack for Mettle

--- i3/.i3/run/slack.sh
+++ i3/.i3/run/slack.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -e
 
-i3-msg 'workspace 7'
+i3-msg 'workspace 4'
 
-[ "$(i3-msg '[title="DisconnectedFactory Slack"] focus')" = '[{"success":true}]' ] || i3-msg "exec --no-startup-id chromium --app=\"https://disconnectedfactory.slack.com\""
+[ "$(i3-msg '[title="Slack"] focus')" = '[{"success":true}]' ] || i3-msg "exec --no-startup-id chromium --app=\"https://mettlebank.slack.com\""
 
+# [ "$(i3-msg '[title="Simba Sleep Slack"] focus')" = '[{"success":true}]' ] || i3-msg "exec --no-startup-id chromium --app=\"https://simbasleep.slack.com\""
+# [ "$(i3-msg '[title="DisconnectedFactory Slack"] focus')" = '[{"success":true}]' ] || i3-msg "exec --no-startup-id chromium --app=\"https://disconnectedfactory.slack.com\""
 # [ "$(i3-msg '[title="Team Eureka Slack"] focus')" = '[{"success":true}]' ] || i3-msg "exec --no-startup-id chromium --app=\"https://team-eurekahq.slack.com\""
