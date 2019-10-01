d01d09b770bffa93a5aea2657bdaf9a775cf1610
Benedict Hartley <spam@chaffin.ch>
2019-10-01 16:40:55 +0100

    Load non-work calendar

--- i3/.i3/run/gcal.sh
+++ i3/.i3/run/gcal.sh
@@ -3,4 +3,4 @@
 i3-msg 'workspace 5'
 
 [ "$(i3-msg '[title="Google Calendar"] focus')" = '[{"success":true}]' ] \
-    || i3-msg "exec --no-startup-id chromium --app=\"https://calendar.google.com\""
+    || i3-msg "exec --no-startup-id chromium --app=\"https://calendar.google.com/calendar/b/2/r\""
